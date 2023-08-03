240a093b39b1be55d23edf68aa4cb212f3ed48fd
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-08-03 19:29:37 -0300

    fix: ajustando product entity

--- src/entities/product/product.ts
+++ src/entities/product/product.ts
@@ -1,10 +1,26 @@
-export interface IProduct {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
+export interface ProductsProps {
+  id: number
+  name: string
+  price: number
+  description: string
 }
 
 export class Product {
-  constructor(public props: IProduct) {}
+  constructor(public props: ProductsProps) {}
+
+  get id() {
+    return this.props.id
+  }
+  get name() {
+    return this.props.name
+  }
+  get price() {
+    return this.props.price
+  }
+  get description() {
+    return this.props.description
+  }
 }
+
+// coloquei get para "simular" o efeito de uma entendidade.
+// se fosse typeorm, não precisaria fazer dessa forma uma vez que esses campos de props já estariam sendo utilizados em algum método para criar tabela.
