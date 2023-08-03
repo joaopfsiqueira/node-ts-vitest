cab758c9bfd92190ddb1a48d922d7498d87472ef
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-08-03 19:31:47 -0300

    fix: ajustando entity de estoque

--- src/entities/stock/stock.ts
+++ src/entities/stock/stock.ts
@@ -1,10 +1,18 @@
-import { IProduct } from '../product/product';
+import { ProductsProps } from '../product/product'
 
 export interface IStock {
-  quantity: number;
-  product: IProduct;
+  quantity: number
+  product: ProductsProps
 }
 
 export class Stock {
   constructor(public stock: IStock) {}
+
+  get quantity(): number {
+    return this.stock.quantity
+  }
+
+  get product(): ProductsProps {
+    return this.stock.product
+  }
 }
