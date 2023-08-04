b3d23f9de56e1dd8d9b87937161ba7e954ac0724
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-08-03 21:23:39 -0300

    feat: criando novo teste para validar se ja existe o produto

--- src/use-cases/create-product.spec.ts
+++ src/use-cases/create-product.spec.ts
@@ -1,10 +1,12 @@
 import { describe, expect, it } from 'vitest'
 import { CreateProduct } from './create-product'
 import { Product } from '../entities/product/product'
+import { InMemoryProductRepository } from '../repositories/in-memory/in-memory-products-repository'
 
 describe('Create Product', () => {
   it('should be able to create an product', () => {
-    const createProduct = new CreateProduct()
+    const productRepository = new InMemoryProductRepository()
+    const createProduct = new CreateProduct(productRepository)
 
     expect(
       createProduct.execute({
@@ -14,5 +16,28 @@ describe('Create Product', () => {
         description: 'Notebook Dell G15 - 8GB RAM - RTX1050 - I5 11H',
       }),
     ).resolves.toBeInstanceOf(Product)
+  })
+
+  it('should not be able to create a product with the same id or already exist', async () => {
+    const productRepository = new InMemoryProductRepository()
+    const createProduct = new CreateProduct(productRepository)
+
+    //cadastro o primeiro produto com id 1
+    await createProduct.execute({
+      id: 1,
+      name: 'Notebook',
+      price: 1000,
+      description: 'Notebook Dell G15 - 8GB RAM - RTX1050 - I5 11H',
+    })
+
+    //tente cadastrar com o mesmo produto
+    expect(
+      createProduct.execute({
+        id: 1,
+        name: 'Notebook',
+        price: 1000,
+        description: 'Notebook Dell G15 - 8GB RAM - RTX1050 - I5 11H',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
