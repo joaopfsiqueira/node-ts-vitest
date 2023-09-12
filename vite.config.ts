f988ef6d94688c2ae3c7df3b9bd066c147ac339a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-12 19:37:32 -0300

    feat: configurando vitest e prettier

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+})
