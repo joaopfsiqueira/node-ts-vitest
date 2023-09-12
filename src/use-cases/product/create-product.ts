677d227442342b2bc35d6fe9417025703a1c6f64
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-12 19:52:28 -0300

    feat: separando use-cases

---
+++ src/use-cases/product/create-product.ts
@@ -0,0 +1,28 @@
+import { ProductRepository } from '../../repositories/products-repository'
+import { Product } from '../../entities/product/product'
+
+interface ICreateProductRequest {
+    id: number
+    name: string
+    price: number
+    description: string
+}
+
+type CreateProductResponse = Product
+
+export class CreateProduct {
+    constructor(private productRepository: ProductRepository) {}
+
+    async execute({ id, name, price, description }: ICreateProductRequest): Promise<CreateProductResponse> {
+        const productAlreadyExists = await this.productRepository.checkIfExist(id)
+
+        if (productAlreadyExists) {
+            throw new Error('Product already exists')
+        }
+
+        const product = new Product({ id, name, price, description })
+
+        await this.productRepository.create(product)
+        return product
+    }
+}
