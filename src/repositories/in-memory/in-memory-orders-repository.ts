8e5d490f2da9486fb3aa5a0950a812a254e3218d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-13 20:19:57 -0300

    feat: criando in memory order

---
+++ src/repositories/in-memory/in-memory-orders-repository.ts
@@ -0,0 +1,22 @@
+import { OrderProps } from '../../entities/order/order'
+export class InMemoryOrderRepository {
+    public items: OrderProps[] = []
+
+    async create(order: OrderProps): Promise<void> {
+        this.items.push(order)
+    }
+
+    async checkIfExist(order_id: number): Promise<boolean> {
+        return this.items.some((objeto) => objeto.id === order_id)
+    }
+
+    async updateProduct(order: OrderProps): Promise<void> {
+        const orderToUpdate = this.items.find((objeto) => objeto.id === order.id)
+
+        if (orderToUpdate) {
+            Object.assign(orderToUpdate, order) //pega tudo dos parametros e atualiza no producto atualizado, usando object.assign.
+        } else {
+            throw new Error(`Product not found`)
+        }
+    }
+}
