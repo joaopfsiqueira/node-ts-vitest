0722ca5f11c5cca21c863d60ee482050294b3c30
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-13 20:24:31 -0300

    feat: inserindo update

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,6 +1,7 @@
 import { ProductsProps } from '../entities/product/product'
 
 export interface ProductRepository {
-  create(product: ProductsProps): Promise<void>
-  checkIfExist(product_id: number): Promise<boolean>
+    create(product: ProductsProps): Promise<void>
+    checkIfExist(product_id: number): Promise<boolean>
+    updateProduct(product: ProductsProps): Promise<void>
 }
