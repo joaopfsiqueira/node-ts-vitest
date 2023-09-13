b752a191a2680f48d034bf1ac28168e4ac6336a3
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-12 21:56:45 -0300

    feat: atualizando in memory procuts

--- src/repositories/in-memory/in-memory-products-repository.ts
+++ src/repositories/in-memory/in-memory-products-repository.ts
@@ -2,13 +2,23 @@ import { ProductsProps } from '../../entities/product/product'
 import { ProductRepository } from '../products-repository'
 
 export class InMemoryProductRepository implements ProductRepository {
-  public items: ProductsProps[] = []
+    public items: ProductsProps[] = []
 
-  async create(product: ProductsProps): Promise<void> {
-    this.items.push(product)
-  }
+    async create(product: ProductsProps): Promise<void> {
+        this.items.push(product)
+    }
 
-  async checkIfExist(product_id: number): Promise<boolean> {
-    return this.items.some((objeto) => objeto.id === product_id)
-  }
+    async checkIfExist(product_id: number): Promise<boolean> {
+        return this.items.some((objeto) => objeto.id === product_id)
+    }
+
+    async updateProduct(product: ProductsProps): Promise<void> {
+        const productToUpdate = this.items.find((objeto) => objeto.id === product.id)
+
+        if (productToUpdate) {
+            Object.assign(productToUpdate, product) //pega tudo dos parametros e atualiza no producto atualizado, usando object.assign.
+        } else {
+            throw new Error(`Product not found`)
+        }
+    }
 }
