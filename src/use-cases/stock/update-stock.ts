9b61c86c9e5384389fa230f2abb9c2eb02755fda
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-13 20:52:07 -0300

    feat: criando use-case de update do stock

---
+++ src/use-cases/stock/update-stock.ts
@@ -0,0 +1,22 @@
+import { StockRepository } from '../../repositories/stock-repository'
+
+interface ICreateStockRequest {
+    quantity: number
+    product_id: number
+}
+
+export class CreateProduct {
+    constructor(private stockRepository: StockRepository) {}
+
+    async execute({ quantity, product_id }: ICreateStockRequest): Promise<any> {
+        const stockAlreadyExists = await this.stockRepository.checkIfExist(product_id)
+
+        if (stockAlreadyExists) {
+            const res = await this.stockRepository.updateStock(quantity, product_id)
+
+            if (res) return { status: 200, message: 'Stock atualizado com sucesso!' }
+        } else {
+            throw new Error('Stock does not exists')
+        }
+    }
+}
