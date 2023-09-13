8ad2dcb1c99f2e023c6c678dcd3b70d27d9c3261
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-13 20:56:28 -0300

    fix: atualizando update para boolean

--- src/repositories/in-memory/in-memory-products-repository.ts
+++ src/repositories/in-memory/in-memory-products-repository.ts
@@ -12,11 +12,12 @@ export class InMemoryProductRepository implements ProductRepository {
         return this.items.some((objeto) => objeto.id === product_id)
     }
 
-    async updateProduct(product: ProductsProps): Promise<void> {
+    async updateProduct(product: ProductsProps): Promise<boolean> {
         const productToUpdate = this.items.find((objeto) => objeto.id === product.id)
 
         if (productToUpdate) {
             Object.assign(productToUpdate, product) //pega tudo dos parametros e atualiza no producto atualizado, usando object.assign.
+            return true
         } else {
             throw new Error(`Product not found`)
         }
