97b805fc91833f5038bc29e74e44c5e756818ad1
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-21 17:30:07 -0300

    fix: corrigindo erro de classe no product

--- src/entities/product/product.spec.ts
+++ src/entities/product/product.spec.ts
@@ -1,15 +1,17 @@
-import { expect, test } from 'vitest';
-import { Product } from './product';
+import { expect, test } from 'vitest'
+import { Product } from './product'
 
 test('create an product with id 1', () => {
-  const product = new Product({
-    id: 1,
-    name: 'Notebook',
-    price: 1000,
-    description: 'Notebook Dell G15 - 8GB RAM - RTX1050 - I5 11H',
-  });
+    const productData = new Product({
+        id: 1,
+        name: 'Notebook',
+        price: 1000,
+        description: 'Notebook Dell G15 - 8GB RAM - RTX1050 - I5 11H',
+    })
 
-  expect(product).toBeInstanceOf(Product); //como estamos instanciando, podemos usar o instanceof
-  expect(product.props.name).toEqual('Notebook');
-  expect(product.props.id).toEqual(1);
-});
+    const product = new Product(productData)
+
+    expect(product).toBeInstanceOf(Product) //como estamos instanciando, podemos usar o instanceof
+    expect(product.props.name).toEqual('Notebook')
+    expect(product.props.id).toEqual(1)
+})
