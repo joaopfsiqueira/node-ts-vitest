4b9372bfefc8522583d6327f877a4a095be4ae46
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-21 17:24:36 -0300

    feat: criando para product que não existe

--- src/use-cases/product/update-product.spec.ts
+++ src/use-cases/product/update-product.spec.ts
@@ -17,4 +17,21 @@ describe('Update Product', () => {
 
         expect(productToUpdate).toEqual({ status: 200, message: 'Product updated successfully' })
     })
+
+    it('should not be able to update an not exist product', async () => {
+        const productRepository = new InMemoryProductRepository(MockProduct)
+        const updateProduct = new UpdateProduct(productRepository)
+
+        const wrapper = async () => {
+            await updateProduct.execute({
+                id: 2,
+                name: 'Notebook novo',
+                price: 1000,
+                description: 'Notebook',
+            })
+        }
+
+        await expect(wrapper).rejects.toThrowError('Product does not exists')
+        //nesse caso, utilizei rejects por ser uma função async, e o await irá retornar uma Promise da função wrapper que chama o execute. Essa forma é melhor para validar retornos de uma função que não seja um objeto ou algo poupável na grande parte do tempo, nesse exemplo, um new Error.
+    })
 })
