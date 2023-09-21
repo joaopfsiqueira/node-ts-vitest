23945cdbb0514c1b7f36fdd400678b99ba002e61
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-21 17:28:37 -0300

    fix: criando instância da classe para completar teste

--- src/entities/stock/stock.spec.ts
+++ src/entities/stock/stock.spec.ts
@@ -3,11 +3,15 @@ import { test, expect } from 'vitest'
 import { Stock } from './stock'
 
 test('insert stock of a product', () => {
-    const stock = new Stock({
+    const stockData = {
         quantity: 10,
-        product_id: 1,
-    })
+        product_id: 123,
+    }
 
-    expect(stock).instanceOf(Stock)
-    expect(stock.stock.quantity).toBe(10)
+    // Crie uma instância da classe Stock
+    const stock = new Stock(stockData)
+
+    // Use expect para verificar os getters
+    expect(stock.quantity).toBe(10) // Verifica se o getter de quantity retorna o valor esperado
+    expect(stock.product_id).toBe(123) // Verifica se o getter de product_id retorna o valor esperado
 })
