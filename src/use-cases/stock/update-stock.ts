7825af5905e9ab6378bc7533df1486e89fd5ca66
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-21 17:14:04 -0300

    fix: corrigindo validação

--- src/use-cases/stock/update-stock.ts
+++ src/use-cases/stock/update-stock.ts
@@ -18,13 +18,9 @@ export class UpdateStock {
 
         if (stockAlreadyExists) {
             const stockToUpdate = await this.stockRepository.returnStockByProductId(product_id)
-            const res = await this.stockRepository.updateStock(stockToUpdate, stock)
+            await this.stockRepository.updateStock(stockToUpdate, stock)
 
-            if (res) {
-                return { status: 200, message: 'Stock updated successfully' }
-            } else {
-                return { status: 400, message: 'Stock not updated' }
-            }
+            return { status: 200, message: 'Stock updated successfully' }
         } else {
             throw new Error('Stock does not exists')
         }
